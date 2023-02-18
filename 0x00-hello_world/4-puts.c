1faf1c5c1e633f3afb70bf92885471320430adbc
Patience123D <Ptncdotse@gmail.com>
2023-02-18 03:34:18 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Printing a statement with puts
+ * Return: the value should be equal to zero
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
