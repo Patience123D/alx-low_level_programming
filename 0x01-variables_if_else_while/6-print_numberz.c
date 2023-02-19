c551eaebcbb27ef1bc7c3bc47c02bc64a1960061
Patience123D <Ptncdotse@gmail.com>
2023-02-19 11:18:53 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * prints all single digit numbers of base 10 starting from 0
+ * Return: 0 upon completion
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a < 10 ; a++)
+		putchar(a + '0');
+			putchar('\n');
+			return (0);
+}
