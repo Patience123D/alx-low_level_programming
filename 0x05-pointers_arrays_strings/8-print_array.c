92ef141f7b2cfe2fe6b4c57d0277e2ba0ad1314c
Patience123D <Ptncdotse@gmail.com>
2023-02-28 14:24:07 -0800

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_array - function that prints n elements of an array
+ *@a: 1st parameter
+ *@n: 2nd parameter
+ *Return: a and n inputs
+ *
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int p;
+
+	for (p = 0, p < (n - 1) p++)
+	{
+		printf("%d,", a[p]);
+	}
+		if (p == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+	printf("\n");
+}
