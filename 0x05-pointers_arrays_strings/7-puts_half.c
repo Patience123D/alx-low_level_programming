1363cd74d5135268555d66bfe9794083cd670ded
Patience123D <Ptncdotse@gmail.com>
2023-02-27 20:08:41 -0800

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *puts_half - function that prints half of a string
+ *@str: parameter
+ *Return; 0
+ *
+ *
+ */
+
+void puts_half(char *str);
+{
+	int p;
+	int half = 0;
+
+	for (p = 0; str[p] != '\0' ; p++)
+		half++;
+	if (half % 2 == 0)
+		for (p = half / 2 ; str[p] != '\0' ; p++)
+			_putchar(str[p]);
+	_putchar('\n');
+	else if (half % 2 != 0)
+		for (p = (half - 1) / 2 ; str[p] != '\0' ; p++)
+			_putchar(str[i]);
+	_putchar('\n');
+}
