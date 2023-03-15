40a6af99341f7b7841b3869d2dd197f8cf429927
Patience123D <Ptncdotse@gmail.com>
2023-03-15 05:40:23 -0700

    It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - executed part of the program
+ *Return: 0
+ *@argc: argument count
+ *@argv: argument vector
+ */
+
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main (int __attribute__((unused))argc, char *argv[]);
+
+
+
+
+
+
+#endif
