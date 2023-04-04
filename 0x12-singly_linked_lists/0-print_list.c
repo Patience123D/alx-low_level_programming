90beab1046d429fd3db88629d9e2ea1de56db300
Patience123D <ptncdotse@gmail.com>
2023-04-04 08:33:22 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - Prints lists
+ * @h: the head
+ * Return: Elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
