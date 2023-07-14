0292983a79a5534309861e8c2f4dec638a258b73
Patience123D <ptncdotse@gmail.com>
2023-07-14 12:30:38 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * create_file - Creates a file with a content
@@ -8,21 +14,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int d, i;
+	int fd, len;
+	ssize_t res_write;
 
 	if (filename == NULL)
 		return (-1);
-	d = open(filename, O_RDWR | O_TRUNC);
-	if (d < 0)
-		d = open(filename, O_WRONLY | O_CREAT, 0600);
-	if (d >= 0)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
 	{
-		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
+		len = 0;
+		while (*(text_content + len) != '\0')
+			len++;
+		res_write = write(fd, text_content, len);
+		if (res_write == -1)
 		{
-			if (write(d, text_content + i, 1) != 1)
-				return (-1);
+			write(1, "fails", 6);
+			return (-1);
 		}
-		close(d);
 	}
+	close(fd);
 	return (1);
 }
