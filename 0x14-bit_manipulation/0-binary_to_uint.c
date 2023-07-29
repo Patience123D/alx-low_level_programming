f4b5c151760971ad90763ed6ab18d91fc9f02484
Patience123D <ptncdotse@gmail.com>
2023-07-29 02:27:28 -0700

    up

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,22 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int n;
-	unsigned int num;
+	unsigned int num = 0;
+	int len = 0;
 
-	num = 0;
-	if (!b)
+	if (b[len] == '\0')
 		return (0);
-	for (n = 0; b[i] != '\0'; n++)
-	{
-		if (b[n] != '0' && b[n] != '1')
-			return (0);
-	}
-	for (n = 0; b[n] != '\0'; n++)
+
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
 		num <<= 1;
-		if (b[n] == '1')
-			num += 1;
+		num += b[len] - '0';
+		len++;
 	}
+
 	return (num);
 }
